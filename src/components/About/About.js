2c551d88b5441b246ef873be1877de8d73097bf1
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-07-01 00:45:41 +0200

    adding: about component with style

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,37 @@
+import {
+  CreativeSection,
+  CreativeInfo,
+  InfoTitle,
+  InfoDir,
+  InfoDesc,
+  InfoTitleSpan,
+  InfoAnchor,
+} from "./AboutStyle";
+
 const About = () => {
-    return (
-        <div>
-            About
-        </div>
-    )
-}
+  return (
+    <CreativeSection>
+      <div className="container">
+        <CreativeInfo>
+          <InfoTitle>
+            <InfoTitleSpan>This is</InfoTitleSpan> Me
+          </InfoTitle>
+          <InfoDir>Creative Director</InfoDir>
+          <InfoDesc>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est{" "}
+            <InfoAnchor href="#">explicabo</InfoAnchor> maxime quibusdam quaerat
+            nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi
+            cupiditate ad veritatis?
+          </InfoDesc>
+          <InfoDesc>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est
+            explicabo maxime quibusdam quaerat nihil aliquid aperiam deleniti,
+            repellendus dolorum. Possimus nisi cupiditate ad veritatis?
+          </InfoDesc>
+        </CreativeInfo>
+      </div>
+    </CreativeSection>
+  );
+};
 
 export default About;
