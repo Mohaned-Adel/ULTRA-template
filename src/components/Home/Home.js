f563032c6279879a3de1b9a32fe0cc0b6e1e92e0
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-06-30 14:46:44 +0200

    adding:NavBar and Home section with Styled-components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,30 @@
+import {
+  HomeSection,
+  HomeInformation,
+  HomeTitle,
+  HomeInfo,
+  HomeDesc,
+  HomeBtn,
+  HomeSpan,
+} from "./HomeStyle";
+
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  return (
+    <HomeSection>
+      <div className="container">
+        <HomeInformation>
+          <HomeTitle>Mohaned Adel</HomeTitle>
+          <HomeInfo>Software Developer</HomeInfo>
+          <HomeDesc>
+            Iam a professional <HomeSpan>UX Designer</HomeSpan> and Front-End Developer
+            creating modern and resposive designs to Web and Mobile. Let us work
+            together. Thank you.
+          </HomeDesc>
+          <HomeBtn className="home-btn">Let's Begin</HomeBtn>
+        </HomeInformation>
+      </div>
+    </HomeSection>
+  );
+};
 
 export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,45 @@
-import { Link } from "react-router-dom";
+
+import {
+  NavbarSection,
+  Logo,
+  LogoText,
+  UlList,
+  ListItem,
+  Anchor,
+  StyledLink,
+} from "./NavbarStyle";
 
 const Navbar = () => {
-    return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/contact">contact</Link>
-        </div>
-    )
-}
+  return (
+    <NavbarSection>
+      <div className="container">
+        <Logo>
+          <LogoText>Ultra Profile</LogoText>
+        </Logo>
+
+        <UlList>
+          <ListItem>
+            <StyledLink to={"/"}>Home</StyledLink>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">Work</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">Portfolio</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">Resume</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">About</Anchor>
+          </ListItem>
+          <ListItem>
+            <StyledLink to={"/contact"}>Contact</StyledLink>
+          </ListItem>
+        </UlList>
+      </div>
+    </NavbarSection>
+  );
+};
 
 export default Navbar;
