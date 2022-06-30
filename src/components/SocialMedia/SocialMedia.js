29d5e5fcfb30892aa56a83cc9673bb65ace8a15a
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-07-01 01:21:39 +0200

    adding: Social component with style

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import {
+  SocialSection,
+  SocialDiv,
+  Icon,
+  SocialParagraph,
+  SocialParSpan,
+} from "./SocialStyle";
+
 const SocialMedia = () => {
+  const [socials, setSocials] = useState([]);
+
+  useEffect(() => {
+    requestData();
+    // eslint-disable-next-line
+  }, []);
+
+  async function requestData() {
+    const result = await fetch("js/data.json");
+    const res = await result.json();
+
+    setSocials(res.social);
+    console.log(socials);
+  }
+
+  const SocialData = socials.map((social) => {
     return (
-        <div>
-            SocialMedia
-        </div>
-    )
-}
+      <SocialDiv key={social.id} id={social.id}>
+        <Icon className={social.icon}></Icon>
+        <SocialParagraph>
+          <SocialParSpan>{social.title}</SocialParSpan>
+          <SocialParSpan info2={true}>{social.body}</SocialParSpan>
+        </SocialParagraph>
+      </SocialDiv>
+    );
+  });
+  return <SocialSection>{SocialData}</SocialSection>;
+};
 
 export default SocialMedia;
